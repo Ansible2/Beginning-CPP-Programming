e5d7c99f20fe2df7501c9bd527e08da1da350b60
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-12-23 09:35:29 -0700

    Learned about pointers and arrays and pointer arithmetic and dynamic memory allocation

--- Learning CPP Beginning Solution/Pointers/main.cpp
+++ Learning CPP Beginning Solution/Pointers/main.cpp
@@ -72,7 +72,8 @@ int main() {
 	temp_ptr = &lowTemp;
 	cout << *temp_ptr << endl;
 	*/
-
+	
+	/*
 	vector<string> stooges{ "Larry","Moe","Curly" };
 	vector<string>* vector_ptr{ nullptr };
 
@@ -86,8 +87,108 @@ int main() {
 		cout << stooge << " ";
 	}
 	cout << endl << endl;
+	*/
 
 
+// Dynamic Memory
+	/*
+	int* int_ptr{ nullptr };
+	int_ptr = new int; // allocates memory on the heap
+	cout << int_ptr << endl;
+	delete int_ptr; // de allocate memory to prevent memory leak
+
+	size_t size(0);
+	double* temp_ptr{ nullptr };
+
+	cout << "How many temps? ";
+	cin >> size;
+	temp_ptr = new double[size];
+
+	delete[] temp_ptr;
+
+	cout << endl;
+	*/
+
+	// arrays and pointers
+	/*
+	int scores[] { 100,95,89 };
+
+	cout << "Value of scores: " << scores << endl;
+
+	// since the name of an array is already an address, you don't need a & to get it
+	int* score_ptr{ scores }; // assigns the (memory location) value of the first element of scores.
+	cout << "Value of score_ptr: " << score_ptr << endl;
+
+	cout << "\nArray subscript notation -----------------------------------------------" << endl;
+	cout << scores[0] << endl; // takes memory location and adds the memory value of one integer (because that's the type of storage it is) and adds it 0 times
+	cout << scores[1] << endl; // adds it 1 time (so + 4 bytes)
+	cout << scores[2] << endl; // adds it 2 times (so + 8 bytes)
+
+	cout << "\nPointer subscript notation -----------------------------------------------" << endl;
+	cout << score_ptr[0] << endl; // takes memory location and adds the memory value (4 bytes) of one integer (because that's the type of storage it is) and adds it 0 times
+	cout << score_ptr[1] << endl; // adds it 1 time (so + 4 bytes)
+	cout << score_ptr[2] << endl; // adds it 2 times (so + 8 bytes)
+
+	// this is much the same way
+	cout << "\nPointer offset notation -----------------------------------------------" << endl;
+	cout << *score_ptr << endl;
+	cout << *(score_ptr + 1) << endl; // get memory location and add the storage value of 1 int
+	cout << *(score_ptr + 2) << endl; // add storage value of 2 ints and then get what that memory address is pointing to
+
+	cout << "\nArray offset notation -----------------------------------------------" << endl;
+	cout << *scores << endl; // get the first memory location of the array
+	cout << *(scores + 1) << endl; // get memory location and add the storage value of 1 int
+	cout << *(scores + 2) << endl; // add storage value of 2 ints and then get what that memory address is pointing to
+	*/
+
+	// pointer arithmetic
+	/*
+	int scores[]{ 100,95,89,68,-1 }; // -1 is a sentinel value that marks the end of our array
+	int* score_ptr{ scores };
+
+	while (*score_ptr != -1) {
+		cout << *score_ptr << endl;
+		score_ptr++; // increment by whatever it is pointing to (in terms of memory size)
+	};
+
+	cout << "\n-------------------------------" << endl;
+	score_ptr = scores;
+
+	while (*score_ptr != -1) {
+		cout << *score_ptr++ << endl; // dereferences pointer (get actual value of what it is pointing to) and THEN increments it
+	};
+
+
+	cout << "\n-------------------------------" << endl;
+	string s1{ "Frank" };
+	string s2{ "Frank" };
+	string s3{ "James" };
+
+	string *p1{&s1};
+	string *p2{&s2};
+	string *p3{&s1};
+
+	cout << boolalpha;
+	cout << p1 << " == " << p2 << ": " << (p1 == p2) << endl; // compares point addresses, not there values
+	cout << p1 << " == " << p3 << ": " << (p1 == p3) << endl; // compares point addresses, not there values
+
+	cout << *p1 << " == " << *p2 << ": " << (*p1 == *p2) << endl; // actual values
+	cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << endl;
+
+
+	cout << "\n-------------------------------" << endl;
+	char name[]{ "Frank" };
+
+	char* char_ptr1{ nullptr };
+	char* char_ptr2{ nullptr };
+
+	char_ptr1 = &name[0];
+	char_ptr2 = &name[3];
+
+	cout << "In the string " << name << ", " << *char_ptr2 << " is " << (char_ptr2 - char_ptr1) << " characters away from " << *char_ptr1 << endl;
+	*/
+
+	// const and pointer
 
 	return 0;
 }
